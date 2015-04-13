3a75c9dad33c26ed3d5bff9882c8192af670d098
Jeanette <kiriian@hotmail.com>
2015-04-13 12:31:58 +0200

    Jeg har sat det til min database

--- src/java/data/DB.java
+++ src/java/data/DB.java
@@ -3,7 +3,7 @@ package data;
 public class DB {
 	public static String DRIVER = "oracle.jdbc.driver.OracleDriver";
 	public static String URL = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
-	public static String ID = "cphpj128";
-	public static String PW = "cphpj128";
+	public static String ID = "cphjb190";
+	public static String PW = "cphjb190";
 }
 
